2b021141bc935aa1e667bcf2699ecc23ca21d7d4
Ricardo Bressan <wkbressan@gmail.com>
2022-11-24 10:48:16 -0300

    buttons

---
+++ paginaportfolio.js
@@ -0,0 +1,3 @@
+//Para linkar algum botão, seria esse código:
+//<a class="efeito efeito-6" href="#LINK AQUI" style="text-decoration:none" type="button">Efeito 6</a>
+//O style="text-decoration:none" é pra retirar o sublinhado do link.
